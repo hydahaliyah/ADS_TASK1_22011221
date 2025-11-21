47b78bf539d7e4ac27b6d7b076fddc115d971f55
hydah aliyah binti hydzelan <hydah_22011221@utp.edu.my>
2025-11-22 00:57:42 +0800

    25_10_16_completed_task_22011221

--- 25_10_16/task1.cpp
+++ 25_10_16/task1.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <string> 
 using namespace std;
+
 struct Node {
     string data;
     Node* next_ptr;
 };
+
 int main() {
     Node node1, node2, node3;
     node1.data = "test1";
@@ -19,28 +22,12 @@ int main() {
     int n;
     cout << "How many times do you wish to traverse: ";
     cin >> n;
+    
     for (int i = 0; i < n; i++) {
         cout << current->data << " ";
         current = current->next_ptr;
     }
     cout << endl;
-/*
-    cout << "One complete cycle: ";
-    Node* start = &node1;
-    current = start;
-    do {
-        cout << current->data << " ";
-        current = current->next_ptr;
-    } while (current != start);
-    cout << endl;
 
-    // starting from different nodes
-    cout << "Starting from node2: ";
-    current = &node2;
-    for (int i = 0; i < 3; i++) {
-        cout << current->data << " ";
-        current = current->next_ptr;
-    }
-    cout << endl;*/
     return 0;
 }
