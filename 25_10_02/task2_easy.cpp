9310b5e597afed73ab60e2d425f34ec2e4c20c8f
hydah aliyah binti hydzelan <hydah_22011221@utp.edu.my>
2025-11-21 23:45:49 +0800

    completed_task_2_22011221

--- 25_10_02/task2_easy.cpp
+++ 25_10_02/task2_easy.cpp
@@ -1,46 +1,73 @@
-//Given a binary 2D array, where each row is sorted. Find the row with the maximum number of 1s. 
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
+
+int findMaxOnesRow(int mat[][10], int rows, int cols) {
+    if (rows <= 0 || cols <= 0) {
+        return -1;
+    }
+
+    int i = 0;
+    int j = cols - 1;
+    int maxRowIndex = -1;
+
+    while (i < rows && j >= 0) {
+        if (mat[i][j] == 1) {
+            maxRowIndex = i;
+            j--;
+        } else {
+            i++;
+        }
+    }
+
+    return maxRowIndex;
+}
+
 int main() {
     int rows, cols;
     cout << "Enter number of rows: ";
     cin >> rows;
     cout << "Enter number of columns: ";
     cin >> cols;
+
+    if (rows > 10 || cols > 10 || rows <= 0 || cols <= 0) {
+        return 1;
+    }
+
     int mat[10][10];
-    cout << "\nEnter the binary matrix (0s and 1s):" << endl;
-    for (int i = 1; i <= rows; i++) {
-        cout << "Row " << i << ": ";
-        for (int j = 1; j <= cols; j++) {
+    
+    cout << "Enter the binary matrix (row by row):\n";
+    for (int i = 0; i < rows; i++) {
+        cout << "Enter Row " << i + 1 << ": ";
+        for (int j = 0; j < cols; j++) {
             cin >> mat[i][j];
         }
     }
-    cout << "\nYour matrix:" << endl;
-    for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= cols; j++) {
+
+    cout << "\nYour matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             cout << mat[i][j] << " ";
         }
         cout << endl;
     }
-    cout << endl;
-    int maxones=0;
-    int maxrow=0;;
-    for (int i=1; i<=rows;i++){
-        int count=0;
-        for(int j=1; j<=cols; j++){
-            if (mat[i][j]==1){
-                count++;
-            }
+
+    int maxRowIndex = findMaxOnesRow(mat, rows, cols);
+
+    if (maxRowIndex == -1) {
+        cout << "None of the rows has a '1' or the matrix is empty." << endl;
+    } else {
+        int maxRow = maxRowIndex + 1;
+        int maxOnes = cols; 
+        for(int j=0; j<cols; ++j){
+            if(mat[maxRowIndex][j] == 0) maxOnes--;
+            else break;
         }
-        if (count>maxones){
-            maxrow=i;
-            maxones=count;
-        }
+
+        cout << "Row " << maxRow << " has the maximum number of ones." << endl;
+        cout << "Number of ones: " << maxOnes << endl;
     }
-    if (maxrow==0){
-        cout << "None of the rows has one" << endl;
-    } else{
-    cout << "Row " << maxrow << " has the maximum num of ones" << endl;
-    cout << "Number of ones: " << maxones << endl;
-    }
+
+    return 0;
 }
