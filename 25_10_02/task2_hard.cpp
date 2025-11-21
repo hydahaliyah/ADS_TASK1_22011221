05211dd7f21249e5a8ce319e6950ecd8a2b738c5
hydah aliyah binti hydzelan <hydah_22011221@utp.edu.my>
2025-11-22 00:01:59 +0800

    Completed_task2(3)_22011221

--- 25_10_02/task2_hard.cpp
+++ 25_10_02/task2_hard.cpp
@@ -1,21 +1,26 @@
-/* to check
-    1)  countX == countO or countX == countO + 1
-    2)  If O is in win condition then check 
-        a) If X also wins, not valid
-        b) If xCount != oCount, not valid
-    3)  If X is in win condition then check if xCount is
-        one more than oCount or not
-    https://www.geeksforgeeks.org/dsa/validity-of-a-given-tic-tac-toe-board-configuration/
-*/
-
 #include <iostream>
 using namespace std;
+
+
+bool checkWin(char board[], char player) {
+    for (int i = 0; i < 9; i += 3) {
+        if (board[i] == player && board[i+1] == player && board[i+2] == player) return true;
+    }
+   
+    for (int j = 0; j < 3; j++) {
+        if (board[j] == player && board[j+3] == player && board[j+6] == player) return true;
+    }
+    
+    if (board[0] == player && board[4] == player && board[8] == player) return true;
+    if (board[2] == player && board[4] == player && board[6] == player) return true;
+    
+    return false;
+}
 
 bool isValidTicTacToe(char board[], bool &xWins, bool &oWins) {
     int countX = 0;
     int countO = 0;
     
-    // Count X and O
     for (int i = 0; i < 9; i++) {
         if (board[i] == 'X'){ 
             countX++;
@@ -24,68 +29,22 @@ bool isValidTicTacToe(char board[], bool &xWins, bool &oWins) {
             countO++;
         }
     }
-//1: X always plays first, so countX = countO or countO+1
+
     if (!(countX == countO || countX == countO + 1)) {
         return false;
     }
 
-    xWins = false;
-    oWins = false;
+    xWins = checkWin(board, 'X');
+    oWins = checkWin(board, 'O');
     
-    // Check rows
-    for (int i = 0; i < 9; i += 3) {
-        //0 = 1 = 2 or 3 = 4 = 5 ...
-        if (board[i] != '-' && board[i] == board[i+1] && board[i+1] == board[i+2]){
-            if (board[i] == 'X'){
-               xWins = true; 
-            }else if(board[i] == 'O'){
-               oWins = true; 
-            } 
-        }
-    }
-
-    // Check columns
-    for (int j = 0; j < 3; j++){
-        //0=3=6 or 1=4=7 ...
-        if (board[j] != '-' && board[j] == board[j+3] && board[j+3] == board[j+6]) {
-            if (board[j] == 'X'){
-               xWins = true; 
-            }else if(board[j] == 'O'){
-               oWins = true; 
-            }
-        }
-    }
-    
-    // Check both diagonals
-    if (board[0] != '-' && board[0] == board[4] && board[4] == board[8]) {
-        if (board[0] == 'X'){ 
-            xWins = true;
-        }
-        else if (board[0] == 'O'){
-            oWins = true;
-        }
-    }
-    if (board[2] != '-' && board[2] == board[4] && board[4] == board[6]) {
-        if (board[2] == 'X'){ 
-            xWins = true;
-        }
-        else if (board[2] == 'O'){
-            oWins = true;
-        }
-            
-    }
-    
-    //2: Both players can't win
     if (xWins && oWins) {
         return false;
     }
     
-    //3: If X wins, countX should be countO + 1
     if (xWins && countX != countO + 1) {
         return false;
     }
     
-    // 4: If O wins, countX==countO
     if (oWins && countX != countO) {
         return false;
     }
@@ -112,8 +71,8 @@ int main() {
     
     if (isValidTicTacToe(board, xWins, oWins)) {
         cout << "\nValid board position!" << endl;
-        if (xWins== true){
-            cout << "X wins";
+        if (xWins == true){
+            cout << "X wins" << endl;
         } else if (oWins == true){
             cout << "O wins!" << endl;
         } else {
